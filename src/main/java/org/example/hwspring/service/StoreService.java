b3d27bcf6fafc605ce7ebf7ee5c7d4cd48486212
Лаисат <lz_256@mail.ru>
2024-05-01 11:46:12 +0300

    comment

---
+++ src/main/java/org/example/hwspring/service/StoreService.java
@@ -0,0 +1,27 @@
+package org.example.hwspring.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@SessionScope
+public class StoreService {
+    private final List<Long> ids;
+
+    public StoreService() {
+        ids = new ArrayList<Long>();
+    }
+
+    public void add(List<Long> ids) {
+        this.ids.addAll(ids);
+    }
+    public List<Long> get() {
+        return Collections.unmodifiableList(ids);
+    }
+}
